e5d6952dda9e236c9d99fe8f1704dc1e46d98110
luis <foreverluiscastro@gmail.com>
2021-04-05 21:35:31 -0400

    added description button to card, havent worked out the JS yet

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const beerCollection = document.querySelector('#beers');
 
 document.addEventListener("DOMContentLoaded", () => {
     fetchBeers()
+    openDescription()
 })
 
 function fetchBeers() {
@@ -22,7 +23,29 @@ function renderSingleBeer(beer) {
     div.innerHTML = `
         <h3>${beer.name}</h3>
         <img src=${beer.image_url} class="beer-avatar"/>
-        <p>${beer.tagline}</p>
+        <p>Tagline:${beer.tagline}</p>
+        <p>ABV%:${beer.abv}</p>
     `
+    let button = document.createElement("button")
+    button.className = "desrciption-button"
+    button.innerText = "Description"
+    button.addEventListener('click', () => {
+        openDescription(beer)
+    })
+    div.appendChild(button)
     beerCollection.appendChild(div)
+}
+
+function openDescription(beers, button) {
+    return fetch("https://api.punkapi.com/v2/beers/", {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        button.innerText = beers.description
+})
 }
