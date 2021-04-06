6ee20cf8479f438cb61755dd8d9ddfa7ae1d2d86
luis <foreverluiscastro@gmail.com>
2021-04-05 20:25:03 -0400

    added fucntionality to JS, updated HTML and CSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+const beerCollection = document.querySelector('#beers');
+
 document.addEventListener("DOMContentLoaded", () => {
-    
+    fetchBeers()
 })
+
+function fetchBeers() {
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(res => res.json())
+    .then(data => {
+        renderAllBeers(data)
+    })
+}
+
+function renderAllBeers(array) {
+    return array.forEach(beer => renderSingleBeer(beer))
+}
+
+function renderSingleBeer(beer) {
+    let div = document.createElement("div")
+    div.className = "card"
+    div.innerHTML = `
+        <h3>${beer.name}</h3>
+        <img src=${beer.image_url} class="beer-avatar"/>
+        <p>${beer.tagline}</p>
+    `
+    beerCollection.appendChild(div)
+}
